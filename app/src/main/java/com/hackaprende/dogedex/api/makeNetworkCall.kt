28350a3528789cfd8cc7f3b906661e7139d1e91e
Jesus <hackaprende@gmail.com>
2021-12-11 17:31:38 -0600

    Generalize network calls

---
+++ app/src/main/java/com/hackaprende/dogedex/api/makeNetworkCall.kt
@@ -0,0 +1,19 @@
+package com.hackaprende.dogedex.api
+
+import com.hackaprende.dogedex.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import java.net.UnknownHostException
+
+suspend fun <T> makeNetworkCall(
+    call: suspend () -> T
+): ApiResponseStatus<T> = withContext(Dispatchers.IO) {
+    try {
+        ApiResponseStatus.Success(call())
+    } catch (e: UnknownHostException) {
+        ApiResponseStatus.Error(R.string.unknown_host_exception_error)
+    } catch (e: Exception) {
+        ApiResponseStatus.Error(R.string.unknown_error)
+    }
+}
--- app/src/main/java/com/hackaprende/dogedex/doglist/DogRepository.kt
+++ app/src/main/java/com/hackaprende/dogedex/doglist/DogRepository.kt
@@ -1,29 +1,17 @@
 package com.hackaprende.dogedex.doglist
 
 import com.hackaprende.dogedex.Dog
-import com.hackaprende.dogedex.R
 import com.hackaprende.dogedex.api.ApiResponseStatus
 import com.hackaprende.dogedex.api.DogsApi.retrofitService
 import com.hackaprende.dogedex.api.dto.DogDTOMapper
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.lang.Exception
-import java.net.UnknownHostException
+import com.hackaprende.dogedex.api.makeNetworkCall
 
 class DogRepository {
 
-    suspend fun downloadDogs(): ApiResponseStatus<List<Dog>> {
-        return withContext(Dispatchers.IO) {
-            try {
-                val dogListApiResponse = retrofitService.getAllDogs()
-                val dogDTOList = dogListApiResponse.data.dogs
-                val dogDTOMapper = DogDTOMapper()
-                ApiResponseStatus.Success(dogDTOMapper.fromDogDTOListToDogDomainList(dogDTOList))
-            } catch (e: UnknownHostException) {
-                ApiResponseStatus.Error(R.string.unknown_host_exception_error)
-            } catch (e: Exception) {
-                ApiResponseStatus.Error(R.string.unknown_error)
-            }
+    suspend fun downloadDogs(): ApiResponseStatus<List<Dog>> = makeNetworkCall {
+            val dogListApiResponse = retrofitService.getAllDogs()
+            val dogDTOList = dogListApiResponse.data.dogs
+            val dogDTOMapper = DogDTOMapper()
+            dogDTOMapper.fromDogDTOListToDogDomainList(dogDTOList)
         }
-    }
 }
