a1857ef3af9aefef480873d47314d0d698437e9e
Jesus Valdez <hackaprende@gmail.com>
2022-02-19 18:16:28 -0600

    Show dog collection

--- app/src/main/java/com/hackaprende/dogedex/doglist/DogListViewModel.kt
+++ app/src/main/java/com/hackaprende/dogedex/doglist/DogListViewModel.kt
@@ -21,8 +21,7 @@ class DogListViewModel : ViewModel() {
     private val dogRepository = DogRepository()
 
     init {
-        downloadUserDogs()
-        downloadDogs()
+        getDogCollection()
     }
 
     fun addDogToUser(dogId: Long) {
@@ -32,17 +31,10 @@ class DogListViewModel : ViewModel() {
         }
     }
 
-    private fun downloadUserDogs() {
+    private fun getDogCollection() {
         viewModelScope.launch {
             _status.value = ApiResponseStatus.Loading()
-            handleResponseStatus(dogRepository.getUserDogs())
-        }
-    }
-
-    private fun downloadDogs() {
-        viewModelScope.launch {
-            _status.value = ApiResponseStatus.Loading()
-            handleResponseStatus(dogRepository.downloadDogs())
+            handleResponseStatus(dogRepository.getDogCollection())
         }
     }
 
@@ -57,7 +49,7 @@ class DogListViewModel : ViewModel() {
 
     private fun handleAddDogToUserResponseStatus(apiResponseStatus: ApiResponseStatus<Any>) {
         if (apiResponseStatus is ApiResponseStatus.Success) {
-            downloadDogs()
+            getDogCollection()
         }
 
         _status.value = apiResponseStatus
--- app/src/main/java/com/hackaprende/dogedex/doglist/DogRepository.kt
+++ app/src/main/java/com/hackaprende/dogedex/doglist/DogRepository.kt
@@ -1,17 +1,58 @@
 package com.hackaprende.dogedex.doglist
 
+import com.hackaprende.dogedex.R
 import com.hackaprende.dogedex.model.Dog
 import com.hackaprende.dogedex.api.ApiResponseStatus
 import com.hackaprende.dogedex.api.DogsApi.retrofitService
 import com.hackaprende.dogedex.api.dto.AddDogToUserDTO
 import com.hackaprende.dogedex.api.dto.DogDTOMapper
 import com.hackaprende.dogedex.api.makeNetworkCall
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class DogRepository {
 
-    suspend fun downloadDogs(): ApiResponseStatus<List<Dog>> = makeNetworkCall {
+    suspend fun getDogCollection(): ApiResponseStatus<List<Dog>> {
+        return withContext(Dispatchers.IO) {
+            val allDogsListResponse = downloadDogs()
+            val userDogsListResponse = getUserDogs()
+
+            if (allDogsListResponse is ApiResponseStatus.Error) {
+                allDogsListResponse
+            } else if (userDogsListResponse is ApiResponseStatus.Error) {
+                userDogsListResponse
+            } else if (allDogsListResponse is ApiResponseStatus.Success &&
+                userDogsListResponse is ApiResponseStatus.Success) {
+                ApiResponseStatus.Success(getCollectionList(allDogsListResponse.data,
+                    userDogsListResponse.data))
+            } else {
+                ApiResponseStatus.Error(R.string.unknown_error)
+            }
+        }
+    }
+
+    private fun getCollectionList(allDogList: List<Dog>, userDogList: List<Dog>) =
+        allDogList.map {
+            if (userDogList.contains(it)) {
+                it
+            } else {
+                Dog(
+                    0, it.index, "", "", "", "",
+                    "", "", "", "", ""
+                )
+            }
+        }
+
+    private suspend fun downloadDogs(): ApiResponseStatus<List<Dog>> = makeNetworkCall {
         val dogListApiResponse = retrofitService.getAllDogs()
+        val dogDTOList = dogListApiResponse.data.dogs
+        val dogDTOMapper = DogDTOMapper()
+        dogDTOMapper.fromDogDTOListToDogDomainList(dogDTOList)
+    }
+
+    private suspend fun getUserDogs(): ApiResponseStatus<List<Dog>> = makeNetworkCall {
+        val dogListApiResponse = retrofitService.getUserDogs()
         val dogDTOList = dogListApiResponse.data.dogs
         val dogDTOMapper = DogDTOMapper()
         dogDTOMapper.fromDogDTOListToDogDomainList(dogDTOList)
@@ -24,12 +65,5 @@ class DogRepository {
         if (!defaultResponse.isSuccess) {
             throw Exception(defaultResponse.message)
         }
-    }
-
-    suspend fun getUserDogs(): ApiResponseStatus<List<Dog>> = makeNetworkCall {
-        val dogListApiResponse = retrofitService.getUserDogs()
-        val dogDTOList = dogListApiResponse.data.dogs
-        val dogDTOMapper = DogDTOMapper()
-        dogDTOMapper.fromDogDTOListToDogDomainList(dogDTOList)
     }
 }
