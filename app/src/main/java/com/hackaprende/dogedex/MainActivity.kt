abd7517123840cffc566eec60e0759209e4914d8
Jesus Valdez <hackaprende@gmail.com>
2022-02-26 11:18:46 -0600

    Add camera permissions

--- app/src/main/java/com/hackaprende/dogedex/MainActivity.kt
+++ app/src/main/java/com/hackaprende/dogedex/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.hackaprende.dogedex
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.hackaprende.dogedex.api.ApiServiceInterceptor
 import com.hackaprende.dogedex.auth.LoginActivity
 import com.hackaprende.dogedex.databinding.ActivityMainBinding
@@ -11,6 +19,18 @@ import com.hackaprende.dogedex.model.User
 import com.hackaprende.dogedex.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.d("TAG", "requestCameraPermission: lalalala")
+            } else {
+                Toast.makeText(this, R.string.camera_permission_rejected_message,
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -30,6 +50,39 @@ class MainActivity : AppCompatActivity() {
 
         binding.dogListFab.setOnClickListener {
             openDogListActivity()
+        }
+
+        requestCameraPermission()
+    }
+
+    private fun requestCameraPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // Open camera
+                    Log.d("TAG", "requestCameraPermission: lalalala")
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                    AlertDialog.Builder(this)
+                        .setTitle(R.string.camera_permission_rationale_dialog_title)
+                        .setMessage(R.string.camera_permission_rationale_dialog_message)
+                        .setPositiveButton(android.R.string.ok) { _, _ ->
+                            requestPermissionLauncher.launch(
+                                Manifest.permission.CAMERA
+                            )
+                        }
+                        .setNegativeButton(android.R.string.cancel) { _, _ ->
+                        }.show()
+                }
+                else -> {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA
+                    )
+                }
+            }
+        } else {
+            // Open camera
         }
     }
 
