076d4c7a05e29e49ea7d08409f6a2b0ab914c9e9
Jesus Valdez <hackaprende@gmail.com>
2022-03-21 15:50:43 -0600

    Add navigation to auth screens

--- app/src/main/java/com/hackaprende/dogedex/auth/AuthNavDestinations.kt
+++ app/src/main/java/com/hackaprende/dogedex/auth/AuthNavDestinations.kt
@@ -1,2 +1,6 @@
 package com.hackaprende.dogedex.auth
 
+object AuthNavDestinations {
+    const val LoginScreenDestination = "login_screen"
+    const val SignUpScreenDestination = "sign_up_screen"
+}
--- app/src/main/java/com/hackaprende/dogedex/auth/AuthScreen.kt
+++ app/src/main/java/com/hackaprende/dogedex/auth/AuthScreen.kt
@@ -1,2 +1,42 @@
 package com.hackaprende.dogedex.auth
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.hackaprende.dogedex.auth.AuthNavDestinations.LoginScreenDestination
+import com.hackaprende.dogedex.auth.AuthNavDestinations.SignUpScreenDestination
+
+@Composable
+fun AuthScreen() {
+    val navController = rememberNavController()
+
+    AuthNavHost(
+        navController = navController
+    )
+}
+
+@Composable
+private fun AuthNavHost(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = LoginScreenDestination
+    ) {
+        composable(route = LoginScreenDestination) {
+            LoginScreen(
+                onRegisterButtonClick = {
+                    navController.navigate(route = SignUpScreenDestination)
+                }
+            )
+        }
+
+        composable(route = SignUpScreenDestination) {
+            SignUpScreen(
+                onNavigationIconClick = { navController.navigateUp() }
+            )
+        }
+    }
+}
