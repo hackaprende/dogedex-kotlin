1a242d319a0d000f0f15a3013bf4d0a37f0a155b
Jesus Valdez <hackaprende@gmail.com>
2022-03-21 12:38:47 -0600

    Add LoginScreen views

--- app/src/main/java/com/hackaprende/dogedex/auth/LoginScreen.kt
+++ app/src/main/java/com/hackaprende/dogedex/auth/LoginScreen.kt
@@ -1,19 +1,23 @@
 package com.hackaprende.dogedex.auth
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.intl.LocaleList
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.hackaprende.dogedex.R
 import com.hackaprende.dogedex.api.ApiResponseStatus
@@ -33,6 +37,7 @@ fun LoginScreen(
 @Composable
 fun Content() {
     val email = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -46,17 +51,68 @@ fun Content() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         AuthField(
+            label = stringResource(id = R.string.email),
+            modifier = Modifier
+                .fillMaxWidth(),
             email = email.value,
             onTextChanged = { email.value = it }
+        )
+
+        AuthField(
+            label = stringResource(id = R.string.password),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            email = password.value,
+            onTextChanged = { password.value = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Button(modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+            onClick = {  }) {
+            Text(
+                stringResource(R.string.login),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Medium
+            )
+        }
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+            text = stringResource(R.string.do_not_have_an_account)
+        )
+
+        Text(
+            modifier = Modifier
+                .clickable(enabled = true, onClick = { })
+                .fillMaxWidth()
+                .padding(16.dp),
+            text = stringResource(R.string.register),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Medium
         )
     }
 }
 
 @Composable
-fun AuthField(email: String, onTextChanged: (String) -> Unit) {
+fun AuthField(
+    label: String,
+    email: String,
+    onTextChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+) {
     OutlinedTextField(
+        modifier = modifier,
+        label = { Text(label) },
         value = email,
         onValueChange = { onTextChanged(it) },
+        visualTransformation = visualTransformation,
     )
 }
 
