cf737fa73eceebe675b6a84f9718e048c8b48158
Jesus Valdez <hackaprende@gmail.com>
2022-03-30 20:23:22 -0600

    Add Unit tests for AuthViewModel

---
+++ app/src/test/java/com/hackaprende/dogedex/AuthViewModelTest.kt
@@ -0,0 +1,85 @@
+package com.hackaprende.dogedex
+
+import com.hackaprende.dogedex.api.ApiResponseStatus
+import com.hackaprende.dogedex.auth.AuthTasks
+import com.hackaprende.dogedex.auth.AuthViewModel
+import com.hackaprende.dogedex.model.User
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+import org.junit.Test
+import org.junit.Assert.*
+
+class AuthViewModelTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var dogedexCoroutineRule = DogedexCoroutineRule()
+
+    @Test
+    fun testLoginValidationsCorrect() {
+        class FakeAuthRepository : AuthTasks {
+            override suspend fun login(email: String, password: String): ApiResponseStatus<User> {
+                return ApiResponseStatus.Success(
+                    User(1, "hackaprende@gmail.com", "")
+                )
+            }
+
+            override suspend fun signUp(
+                email: String,
+                password: String,
+                passwordConfirmation: String
+            ): ApiResponseStatus<User> {
+                return ApiResponseStatus.Success(
+                    User(1, "", "")
+                )
+            }
+
+        }
+
+        val viewModel = AuthViewModel(
+            authRepository = FakeAuthRepository()
+        )
+
+        viewModel.login("", "test1234")
+
+        assertEquals(R.string.email_is_not_valid,
+            viewModel.emailError.value)
+
+        viewModel.login("hackaprende@gmail.com", "")
+
+        assertEquals(R.string.password_must_not_be_empty,
+            viewModel.passwordError.value)
+    }
+
+    @Test
+    fun testLoginStatesCorrect() {
+        val fakeUser = User(
+            1, "hackaprende@gmail.com",
+            ""
+        )
+        class FakeAuthRepository : AuthTasks {
+            override suspend fun login(email: String, password: String): ApiResponseStatus<User> {
+                return ApiResponseStatus.Success(fakeUser)
+            }
+
+            override suspend fun signUp(
+                email: String,
+                password: String,
+                passwordConfirmation: String
+            ): ApiResponseStatus<User> {
+                return ApiResponseStatus.Success(
+                    User(1, "", "")
+                )
+            }
+
+        }
+
+        val viewModel = AuthViewModel(
+            authRepository = FakeAuthRepository()
+        )
+
+        viewModel.login("hackaprende@gmail.com", "test1234")
+
+        assertEquals(fakeUser.email, viewModel.user.value?.email)
+    }
+}
